276cc4bbac504e725092efdf830ce6bb5570fce6
takara111 <taka160907@gmail.com>
2019-09-18 16:56:27 +0900

    first commit

--- Test-5-2/Pods/Target Support Files/Pods-Test-5-2/Pods-Test-5-2-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Test_5_2 : NSObject
-@end
-@implementation PodsDummy_Pods_Test_5_2
-@end
--- Test-5-2/Pods/Target Support Files/Pods-Test-5-2Tests/Pods-Test-5-2Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Test_5_2Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Test_5_2Tests
-@end
